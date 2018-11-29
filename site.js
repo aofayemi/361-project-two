64c2ecaf9702d5b3eb9b65c5ca7ec61f7561c825
aofayemi <42700790+aofayemi@users.noreply.github.com>
2018-11-29 17:54:49 -0600

    Remove unwanted elements

--- site.js
+++
@@ -1,7 +0,0 @@
-
-var headingEl = document.querySelector("#heading");
-            headingEl.style.color = "orange";
-
-            a[href*="Linkdin"] {
-                color: purple;
-headingEl.style.backgroundColor = "black";
