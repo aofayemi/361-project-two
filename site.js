c26dbcd20500c4866667d1e58f2b20bde6549f0f
aofayemi <42700790+aofayemi@users.noreply.github.com>
2018-11-29 17:43:33 -0600

    add JS tags

---
+++ site.js
@@ -0,0 +1,7 @@
+
+var headingEl = document.querySelector("#heading");
+            headingEl.style.color = "orange";
+
+            a[href*="Linkdin"] {
+                color: purple;
+headingEl.style.backgroundColor = "black";
